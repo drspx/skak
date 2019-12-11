e33f371939b1e9b1bc6dd623fe01a301d1ab4fba
alkj <s165477@student.dtu.dk>
2019-12-11 01:34:47 +0100

    King

--- src/main/java/pieces/King.java
+++ src/main/java/pieces/King.java
@@ -1,4 +1,63 @@
 package pieces;
 
-public class King {
+import board.Board;
+import board.BoardUtils;
+import board.Move;
+import board.Move.AttackMove;
+import board.Move.MajorMove;
+import board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class King extends Piece{
+
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9 };
+
+    King(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int currentCandidateOffset: CANDIDATE_MOVE_COORDINATES) {
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
+
+            if (isFirstColumnExclusion(this.piecePosition,currentCandidateOffset) ||
+                    isEigthColumnExclusion(this.piecePosition,currentCandidateOffset)){
+                continue;
+            }
+
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                if (!candidateDestinationTile.isTileOccupied()){
+                    legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
+                } else{
+                    final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                    final Alliance pieceAlliance = pieceAtDestination.getPieceAllience();
+                    if (this.PieceAllience != pieceAlliance){
+                        legalMoves.add(new AttackMove(board,this,candidateDestinationCoordinate,pieceAtDestination));
+                    }
+                }
+            }
+
+        }
+
+
+        return ImmutableList.copyOf(legalMoves);
+    }
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] &&(candidateOffset== -9 || candidateOffset==-1 ||
+                candidateOffset==7 );
+    }
+
+    private static boolean isEigthColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.SECOND_COLUMN[currentPosition] && (candidateOffset==-7||candidateOffset==1||
+                candidateOffset==9);
+    }
 }
