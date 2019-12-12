f60e6d1520a21b22b867c167ef4036b3e81d1e62
alkj <s165477@student.dtu.dk>
2019-12-12 13:39:45 +0100

    move

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -80,10 +80,20 @@ public abstract class Piece {
             public boolean isKing() {
                 return false;
             }
+
+            @Override
+            public boolean isRook() {
+                return false;
+            }
         },
         KNIGHT("N") {
             @Override
             public boolean isKing() {
+                return false;
+            }
+
+            @Override
+            public boolean isRook() {
                 return false;
             }
         },
@@ -92,11 +102,21 @@ public abstract class Piece {
             public boolean isKing() {
                 return false;
             }
+
+            @Override
+            public boolean isRook() {
+                return false;
+            }
         },
         ROOK("R") {
             @Override
             public boolean isKing() {
                 return false;
+            }
+
+            @Override
+            public boolean isRook() {
+                return true;
             }
         },
         QUEEN("Q") {
@@ -104,11 +124,21 @@ public abstract class Piece {
             public boolean isKing() {
                 return false;
             }
+
+            @Override
+            public boolean isRook() {
+                return false;
+            }
         },
         KING("K") {
             @Override
             public boolean isKing() {
                 return true;
+            }
+
+            @Override
+            public boolean isRook() {
+                return false;
             }
         };
 
@@ -124,5 +154,7 @@ public abstract class Piece {
             return this.pieceName;
         }
         public abstract boolean isKing();
+
+        public abstract boolean isRook();
     }
 }
